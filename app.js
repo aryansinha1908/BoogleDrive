6cab2af0801a9cf1163006c936b59d8ae7197770
KabootarGod <aryansinha1908@gmail.com>
2026-01-29 14:56:00 +0530

    Removed ejs

--- app.js
+++ app.js
@@ -12,15 +12,11 @@ const app = express();
 
 connectToDB();
 
-app.use(express.json())
-app.use(express.urlencoded({ extended:true }))
 app.use(cookieParser())
 app.use("/file", rateLimit({
     windowMs: 15 * 60 * 1000,
     max: 100
 }))
-
-app.set('view engine', 'ejs')
 
 app.use('/', indexRouter);
 app.use('/user', userRouter);
